1225f0a374e11346bb4233624ccedb35626b6eec
Nurin Fazil <nurinfazil@hotmail.com>
2022-03-20 20:55:02 -0400

    Removed second example

--- pub/js/examples.js
+++ pub/js/examples.js
@@ -2,6 +2,3 @@
 
 const avatarCreator = new AvatarMaker();
 avatarCreator.createGenerator(".avatar-generator")
-
-const avatarCreator2 = new AvatarMaker()
-avatarCreator2.createGenerator(".avatar-generator2")
